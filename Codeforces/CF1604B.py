fc6be20e5f737598df5e99524343dadc93c06067
Nickel-Angel <1239004072@qq.com>
2021-11-01 10:45:19 +0800

    update

---
+++ Codeforces/CF1604B.py
@@ -0,0 +1,20 @@
+# @author Nickel_Angel (1239004072@qq.com)
+# @copyright Copyright (c) 2021
+
+def max(a, b):
+    if (a > b):
+        return a
+    return b
+
+def solve():
+    n, k = map(int, input().split())
+    x = 0
+    while (1 << x) < k:
+        x += 1
+    print(x + max(0, (n - (1 << x) + k - 1) // k))
+    return
+
+t = int(input())
+while t:
+    t -= 1
+    solve()
